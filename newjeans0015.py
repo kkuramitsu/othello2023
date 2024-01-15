d18026992156859a4ae0d09e8ccc0055da00b4ff
azukidayo <150655198+azukidayo@users.noreply.github.com>
2024-01-15 16:00:48 +0900

    Update newjeans0015.py

--- newjeans0015.py
+++ newjeans0015.py
@@ -1,1 +1,16 @@
-print("Hello World")
+import random
+
+class ringoAI(OthelloAI):
+    def __init__(self, face, name):
+        self.face = face
+        self.name = name
+
+    def move(self, board, color: int)->tuple[int, int]:
+        """
+        ボードが与えられたとき、どこに置くか(row,col)を返す
+        """
+        valid_moves = get_valid_moves(board, color)
+        # ランダムに選ぶ
+        selected_move = random.choice(valid_moves)
+        return selected_move
+
