52dec772ec73830268310a4f3c84422954d7bb54
snow61 <134479946+snow61@users.noreply.github.com>
2024-01-15 15:54:17 +0900

    snow61.py

--- snow61.py
+++ snow61.py
@@ -1,1 +1,9 @@
-print("Hello,World!")
+class SnowAI(OthelloAI):
+    def __init__(self, face, name):
+        self.face = face
+        self.name = name
+
+    def move(self, board: np.array, piece: int)->tuple[int, int]:
+        valid_moves = get_valid_moves(board, piece)
+        return valid_moves[-1]
+
