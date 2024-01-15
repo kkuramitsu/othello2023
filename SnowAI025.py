72cccee50df437f754b2ec8fec08670f03e5c685
snow61 <134479946+snow61@users.noreply.github.com>
2024-01-15 16:09:32 +0900

    Create SnowAI025.py

---
+++ SnowAI025.py
@@ -0,0 +1,8 @@
+class SnowAI(OthelloAI):
+    def __init__(self, face, name):
+        self.face = face
+        self.name = name
+
+    def move(self, board: np.array, piece: int)->tuple[int, int]:
+        valid_moves = get_valid_moves(board, piece)
+        return valid_moves[-1]
