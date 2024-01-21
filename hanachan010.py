08376212cada60727433da9749aac0f38108a712
Ayangoooo <151501277+Ayangoooo@users.noreply.github.com>
2024-01-21 15:43:09 +0900

    Update hanachan010.py

--- hanachan010.py
+++ hanachan010.py
@@ -199,7 +199,7 @@ def game(player1: OthelloAI, player2: OthelloAI,N=6):
 class minimaxAI(OthelloAI):
     def __init__(self, face, name):
         self.face = '🐈'
-        self.name = '猫'
+        self.name = 'ねこ'
 
 depth = 3
 
