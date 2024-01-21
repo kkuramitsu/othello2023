385081a34d6697b343b2c11de271df6a0e867fda
rseaaa <150654507+rseaaa@users.noreply.github.com>
2024-01-22 02:31:03 +0900

    Update jin051.py

--- jin051.py
+++ jin051.py
@@ -1,8 +1,15 @@
 from othello2023.othello import *
 
 class mizukikun(OthelloAI):
-
     def __init__(self):
+        self.face = '💧' # 自分の好きな絵文字
+        self.name = '瑞稀' # 自分の好きな名前
+
+
+class OchibiAI(othello.AI):
+
+    def __init__(self, board):
+        super().__init__(board)
         self.face = "○"
 
     def _get_next_moves(self, board):
@@ -31,20 +38,21 @@ class mizukikun(OthelloAI):
             piece: 自分の石の色
 
         Returns:
-            石を置いたマス目の座標
+        石を置いたマス目の座標
         """
         next_moves = self._get_next_moves(board)
-        if next_moves:
+        if next_moves and board.turn == piece:
             best_move = max(next_moves, key=self._get_score)
             return best_move
         return None
 
 
 board = othello.Board()
-ai = mizukikun(board)
+ai = OchibiAI(board)
 
 while board.is_game_over() is False:
     move = ai.move(board, board.turn)
     board.play_move(move)
 
 print(board)
+
