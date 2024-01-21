f265012a945c14302a3c43ef0abee88771a89754
rseaaa <150654507+rseaaa@users.noreply.github.com>
2024-01-22 02:02:59 +0900

    Update jin051.py

--- jin051.py
+++ jin051.py
@@ -2,7 +2,7 @@ from othello2023.othello import *
 
 class mizukikun(OthelloAI):
 
-    def __init__(self,face,name):
+    def __init__(self):
         self.face = '💧' # 自分の好きな絵文字
         self.name = '瑞稀' # 自分の好きな名前
 
