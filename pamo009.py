8739b2754bb21dffcf85bdf2bbc0c5b1ffffe187
menmen888 <140218204+menmen888@users.noreply.github.com>
2024-01-21 18:11:35 +0900

    Update pamo009.py

--- pamo009.py
+++ pamo009.py
@@ -194,7 +194,7 @@ def game(player1: OthelloAI, player2: OthelloAI,N=6):
     comment(player1, player2, board)
 
 class PamoAI(OthelloAI):
-    def __init__(self, face, name):
+    def __init__(self):
         self.face = '🐁'
         self.name = 'パモ'
 
